2e8d8e273fa229663c9818e807193e82f57de3ad
harukakubota-bio <harukakubota.bio@gmail.com>
2026-03-14 21:40:59 +0900

    Solve PROT:Translating RNA into Protein

---
+++ Bioinformatics Stronghold/PROT.py
@@ -0,0 +1,43 @@
+RNA_CODON_TABLE = {
+    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
+    'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
+    'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
+    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'UAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'UAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'UGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
+}
+
+def translate_rna_to_protein(rna_seq):
+    protein = ""
+
+    for i in range(0,len(rna_seq), 3):
+        codon = rna_seq[i:i+3]
+        amino_acid = RNA_CODON_TABLE[codon]
+
+        if amino_acid == "Stop":
+            break
+
+        protein += amino_acid
+    
+    return protein
+
+if __name__ == "__main__":
+    file_path = "Bioinformatics Stronghold/rosalind_prot.txt"
+    with open(file_path,"r", encoding="utf-8") as f:
+        rna_seq = f.read().strip()
+    
+    result = translate_rna_to_protein(rna_seq)
+    output_path = "Bioinformatics Stronghold/rasalind_prot_answer.txt"
+    with open(output_path,"w", encoding="utf-8") as f:
+        f.write(result)
+    
